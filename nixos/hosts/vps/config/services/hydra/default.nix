8d10e58898292ace0cd8af3e9fe62e6c9cb67b8c
TymanWasTaken <ty@myriation.xyz>
2026-03-12 02:08:42 -0600

    More hydra work

--- nixos/hosts/vps/config/services/hydra/default.nix
+++ nixos/hosts/vps/config/services/hydra/default.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, lib, ... }:
 {
     services.hydra = {
         enable = true;
@@ -14,9 +14,39 @@
             <webhooks>
                 Include ${config.sops.templates."hydra/webhooks.conf".path}
             </webhooks>
+
+            <runcommand>
+                job = infra:release:vps
+                command = ln -s $HYDRA_JSON /var/lib/hydra/infra-release-vps.json && systemctl start hydra-nixos-deploy.service
+            </runcommand>
         '';
-        debugServer = true;
     };
+
+    security.polkit.extraConfig = ''
+        // Allow hydra-queue-runner to run the deployment unit
+        polkit.addRule(function(action, subject) {
+            if (
+                action.id == "org.freedesktop.systemd1.manage-units"
+                && action.lookup("unit") == "hydra-nixos-deploy.service"
+                && subject.user == "hydra-queue-runner"
+            ) {
+                return polkit.Result.YES;
+            }
+        });
+    '';
+    systemd.services.hydra-nixos-deploy = {
+        script = ''
+            cat /var/lib/hydra/infra-release-vps.json
+        '';
+
+        serviceConfig = {
+            User = "root";
+            Group = "root";
+            Type = "oneshot";
+        };
+    };
+
+    environment.etc."hydra/infra.spec.json".source = ./infra.spec.json;
 
     systemd.services.hydra-init = {
         bindsTo = [ "container@postgres.service" ];
