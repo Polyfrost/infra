d82f104b85c09024e362a6abdcc5c38f238f715e
LynithDev <61880709+LynithDev@users.noreply.github.com>
2026-07-16 19:19:45 +0200

    Update plus prod

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
             };
         };
         # Production tracks a pinned release tag. Bump this tag manually
-        plus.url = "github:Polyfrost/plus-backend/v1.0.0-rc.1";
+        plus.url = "github:Polyfrost/plus-backend/v1.0.0";
         # Staging tracks HEAD.
         plus-staging.url = "github:Polyfrost/plus-backend";
         ursa-minor = {
