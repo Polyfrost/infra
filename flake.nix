0c9ed6a4352925f736a49fc8db6e52d93ed58ba2
Julian Chang <wyvest@polyfrost.org>
2026-07-31 13:03:26 +0700

    Bump plus-backend prod to v1.1.4

--- flake.nix
+++ flake.nix
@@ -59,7 +59,7 @@
             };
         };
         # Production tracks a pinned release tag. Bump this tag manually
-        plus.url = "github:Polyfrost/plus-backend/v1.1.3";
+        plus.url = "github:Polyfrost/plus-backend/v1.1.4";
         # Staging tracks HEAD.
         plus-staging.url = "github:Polyfrost/plus-backend";
         ursa-minor = {
