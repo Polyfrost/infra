61be8c48fb7215367decaea5ff083b16f5288364
Julian Chang <wyvest@polyfrost.org>
2026-08-09 00:43:19 +0700

    update plus backend staging

--- nixos/hosts/vps/config/services/plus/default.nix
+++ nixos/hosts/vps/config/services/plus/default.nix
@@ -43,6 +43,7 @@ let
                     "a5331404-0e77-440e-8bef-24c071dac1ae"
                     "f247be7c-5b82-41c6-9148-793ded77e71f"
                 ];
+                SPECIAL_CHAT_AUTO_REPLY = ''hey, we're the owners of oneclient. thank you so much for using our open-source products. if you have any suggestions or questions or issues you're having, you can join our discord at discord.gg/polyfrost. also, you can message us directly here! you can only send 1 message per 3 days, so be wise with what you say (but feel free to send us anything you feel is "dumb", we like hearing stuff like that)'';
             };
         };
     };
